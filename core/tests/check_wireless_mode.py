47771d8f830a66b5e8899c395f6f2f9114164f47
Kevin Nordnes <kevinnordnes@proton.me>
2023-04-13 09:12:29 +0200

    fix

--- core/tests/check_wireless_mode.py
+++ core/tests/check_wireless_mode.py
@@ -3,10 +3,15 @@ import subprocess
 
 def check_wireless_mode():
     # Run the iwconfig command and capture the output
-    output = subprocess.run(['iwconfig'], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    completed_process = subprocess.run(['iwconfig'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    # Check if there was an error running the command
+    if completed_process.returncode != 0:
+        print(f"Error running iwconfig: {completed_process.stderr.decode().strip()}")
+        return None
 
     # Convert the output to a string and split it into lines
-    output = output.decode('utf-8')
+    output = completed_process.stdout.decode('utf-8')
     lines = output.split('\n')
 
     # Search for the wireless mode in the output
