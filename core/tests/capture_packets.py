d241a19478f127b7c0286610938967939b685819
Kevin Nordnes <kevinnordnes@proton.me>
2023-04-13 15:42:11 +0200

    added test

--- core/tests/capture_packets.py
+++ core/tests/capture_packets.py
@@ -1,8 +1,9 @@
-from scapy.sendrecv import sniff, wrpcap
-from core.utils.host import get_wireless_mode, set_wireless_mode
-from configparser import ConfigParser
-import os
+import asyncio
+
 import pyrcrack
+
+from rich.console import Console
+from rich.prompt import Prompt
 
 
 async def capture_packets():
@@ -17,4 +18,23 @@ async def capture_packets():
                 print(aps)
 
 
-capture_packets()
+async def scan_for_targets():
+    """Scan for targets, return json."""
+    console = Console()
+    console.clear()
+    console.show_cursor(False)
+    airmon = pyrcrack.AirmonNg()
+
+    interface = Prompt.ask(
+        'Select an interface',
+        choices=[a['interface'] for a in await airmon.interfaces])
+
+    async with airmon(interface) as mon:
+        async with pyrcrack.AirodumpNg() as pdump:
+            async for result in pdump(mon.monitor_interface):
+                console.clear()
+                console.print(result.table)
+                await asyncio.sleep(2)
+
+
+asyncio.run(scan_for_targets())
