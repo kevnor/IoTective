4878edde7164d7921a7c5af60d5e4c8af578c361
Kevin Nordnes <kevinnordnes@proton.me>
2023-04-13 15:50:01 +0200

    test

--- core/tests/capture_packets.py
+++ core/tests/capture_packets.py
@@ -18,23 +18,4 @@ async def capture_packets():
                 print(aps)
 
 
-async def scan_for_targets():
-    """Scan for targets, return json."""
-    console = Console()
-    console.clear()
-    console.show_cursor(False)
-    airmon = pyrcrack.AirmonNg()
-
-    interface = Prompt.ask(
-        'Select an interface',
-        choices=[a['interface'] for a in await airmon.interfaces])
-
-    async with airmon(interface) as mon:
-        async with pyrcrack.AirodumpNg() as pdump:
-            async for result in pdump(mon.monitor_interface):
-                console.clear()
-                console.print(result.table)
-                await asyncio.sleep(2)
-
-
-asyncio.run(scan_for_targets())
+asyncio.run(capture_packets())
