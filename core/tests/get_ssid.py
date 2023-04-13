dd76a059bf61bb7704ec4792f186e89177ebe819
Kevin Nordnes <kevinnordnes@proton.me>
2023-04-13 14:12:22 +0200

    minor changes

--- core/tests/get_ssid.py
+++ core/tests/get_ssid.py
@@ -5,10 +5,14 @@ def get_wifi_ssid():
 
     if not ssid:
         print("Could not determine SSID of connected Wi-Fi router. Are you sure you are connect over Wi-Fi?")
-        answer = input("Enter SSID manually? (Y/n)")
-        if answer.upper() == "Y" or answer == "":
-            ssid = input("SSID (the name of your Wi-Fi network): ")
 
+        while True:
+            answer = input("Enter SSID manually? (Y/n)")
+            if answer.upper() == "Y" or answer == "":
+                ssid = input("SSID (the name of your Wi-Fi network): ")
+                break
+            elif answer.upper() == "N":
+                return False
     return ssid
 
 
